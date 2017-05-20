ae0ba6b5e804a4162e1d7290636d64e7a92f3aba
Tobias Davis <accounts@davistobias.com>
2017-05-19 23:08:18 -0500

    add error logging

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ gpio.on('change', function(channel, value) {
 	console.log('channel ' + channel + ' value is now ' + value)
 })
 
-gpio.setup(18, gpio.DIR_IN, gpio.EDGE_BOTH)
+gpio.setup(18, gpio.DIR_IN, gpio.EDGE_BOTH, err => {
+	console.log('error on setup', err)
+})
 
 console.log('watching channel 18')
