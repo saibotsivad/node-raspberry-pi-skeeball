3bdfc836c005bda88c8e7b388419427c6eba23ce
Tobias Davis <accounts@davistobias.com>
2017-05-19 22:39:30 -0500

    log when started, use different channel

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const gpio = require('rpi-gpio')
 
 gpio.on('change', function(channel, value) {
-	console.log('Channel ' + channel + ' value is now ' + value)
+	console.log('channel ' + channel + ' value is now ' + value)
 })
 
-gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH)
+gpio.setup(18, gpio.DIR_IN, gpio.EDGE_BOTH)
+
+console.log('watching channel 18')
